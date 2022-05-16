d87483b530edabdaa41bc9c3e66f6183f378cad9
dingsheng <dingduoduo0916@gmail.com>
2022-05-16 22:56:03 +0800

    feat: firs_of_array

---
+++ src/02_medium/04_first_of_array/template.ts
@@ -0,0 +1,16 @@
+/**
+ * 实现一个通用First<T>，它接受一个数组T并返回它的第一个元素的类型。
+ */
+type First1<T extends any[]> = T extends [] ? never : T[0]
+// 通过‘length'属性的索引签名来检查数组的长度
+type First2<T extends any[]> = T['length'] extends 0 ? never : T[0]
+// extends union
+type First3<T extends any[]> = T[0] extends T[number] ? T[0] : never
+type First<T extends any[]> = T extends [infer First, ...infer Rest] ? First : never 
+
+function firstOfArray(arr) {
+    const [first, ...rest] = arr
+    if(first === undefined) return 'never'
+    // if(arr.length === 0) return 'never'
+    return arr[0];
+}
